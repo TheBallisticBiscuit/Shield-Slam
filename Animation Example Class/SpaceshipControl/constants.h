ec7e68aa86ebe274ec1a57733dcbdc7081607d47
Noah Newell <newellnd1@gcc.edu>
2016-10-07 00:33:45 -0400

    Getting the project onto bitbucket

---
+++ Animation Example Class/SpaceshipControl/constants.h
@@ -0,0 +1,104 @@
+// Programming 2D Games
+// Copyright (c) 2011 by: 
+// Charles Kelly
+// Chapter 5 constants.h v1.0
+#ifndef _CONSTANTS_H            // Prevent multiple definitions if this 
+#define _CONSTANTS_H            // file is included in more than one place
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+
+//-----------------------------------------------
+// Useful macros
+//-----------------------------------------------
+// Safely delete pointer referenced item
+#define SAFE_DELETE(ptr)       { if (ptr) { delete (ptr); (ptr)=NULL; } }
+// Safely release pointer referenced item
+#define SAFE_RELEASE(ptr)      { if(ptr) { (ptr)->Release(); (ptr)=NULL; } }
+// Safely delete pointer referenced array
+#define SAFE_DELETE_ARRAY(ptr) { if(ptr) { delete [](ptr); (ptr)=NULL; } }
+// Safely call onLostDevice
+#define SAFE_ON_LOST_DEVICE(ptr)    { if(ptr) { ptr->onLostDevice(); } }
+// Safely call onResetDevice
+#define SAFE_ON_RESET_DEVICE(ptr)   { if(ptr) { ptr->onResetDevice(); } }
+#define TRANSCOLOR  SETCOLOR_ARGB(0,255,0,255)  // transparent color (magenta)
+
+//-----------------------------------------------
+//                  Constants
+//-----------------------------------------------
+// graphic images
+const char JPO_IMAGE[]   = "pictures\\JPo Sheet.png"; //ship.png";   // spaceship
+
+// window
+const char CLASS_NAME[] = "Animation Example";
+const char GAME_TITLE[] = "Animation Example";
+const bool FULLSCREEN = false;              // windowed or fullscreen
+const UINT GAME_WIDTH =  640;               // width of game in pixels
+const UINT GAME_HEIGHT = 480;               // height of game in pixels
+
+const float GREEN_KNIGHT_ANIMATION_DELAY = 0.2f;    // time between frames of ship animation
+
+
+//Player 1 Cel
+const int  GREEN_KNIGHT_COLS = 6;
+const int  GREEN_KNIGHT_WIDTH = 384;
+const int  GREEN_KNIGHT_HEIGHT = 256;
+//Player 1 actions
+const int GREEN_KNIGHT_LOOKING_RIGHT_START = 12;			//1st row
+const int GREEN_KNIGHT_LOOKING_RIGHT_END = 12;
+const int GREEN_KNIGHT_LOOKING_LEFT_START = 16;				//2nd row
+const int GREEN_KNIGHT_LOOKING_LEFT_END = 16;
+const int GREEN_KNIGHT_WALKING_RIGHT_START = 12;
+const int GREEN_KNIGHT_WALKING_RIGHT_END = 16;
+const int GREEN_KNIGHT_WALKING_LEFT_START = 18;
+const int GREEN_KNIGHT_WALKING_LEFT_END = 22;
+const int GREEN_KNIGHT_WALKING_UP_START = 6;
+const int GREEN_KNIGHT_WALKING_UP_END = 11;
+const int GREEN_KNIGHT_WALKING_DOWN_START = 0;
+const int GREEN_KNIGHT_WALKING_DOWN_END = 5;
+
+const float GREEN_KNIGHT_SPEED = 90.5f;
+
+const int RED_KNIGHT_LOOKING_RIGHT_START = 12;			//1st row
+const int RED_KNIGHT_LOOKING_RIGHT_END = 12;
+const int RED_KNIGHT_LOOKING_LEFT_START = 16;				//2nd row
+const int RED_KNIGHT_LOOKING_LEFT_END = 16;
+const int RED_KNIGHT_WALKING_RIGHT_START = 12;
+const int RED_KNIGHT_WALKING_RIGHT_END = 16;
+const int RED_KNIGHT_WALKING_LEFT_START = 18;
+const int RED_KNIGHT_WALKING_LEFT_END = 22;
+const int RED_KNIGHT_WALKING_UP_START = 6;
+const int RED_KNIGHT_WALKING_UP_END = 11;
+const int RED_KNIGHT_WALKING_DOWN_START = 0;
+const int RED_KNIGHT_WALKING_DOWN_END = 5;
+
+const float RED_KNIGHT_SPEED = 90.5f;
+
+const int  RED_KNIGHT_COLS = 6;
+const int  RED_KNIGHT_WIDTH = 384;
+const int  RED_KNIGHT_HEIGHT = 256;
+
+const float RED_KNIGHT_ANIMATION_DELAY = 0.2f;
+// game
+const double PI = 3.14159265;
+const float FRAME_RATE  = 200.0f;               // the target frame rate (frames/sec)
+const float MIN_FRAME_RATE = 10.0f;             // the minimum frame rate
+const float MIN_FRAME_TIME = 1.0f/FRAME_RATE;   // minimum desired time for 1 frame
+const float MAX_FRAME_TIME = 1.0f/MIN_FRAME_RATE; // maximum time used in calculations
+
+// key mappings
+// In this game simple constants are used for key mappings. If variables were used
+// it would be possible to save and restore key mappings from a data file.
+const UCHAR ESC_KEY      = VK_ESCAPE;       // escape key
+const UCHAR ALT_KEY      = VK_MENU;         // Alt key
+const UCHAR ENTER_KEY    = VK_RETURN;       // Enter key
+const UCHAR GREEN_KNIGHT_LEFT_KEY    = VK_LEFT;     // left arrow
+const UCHAR GREEN_KNIGHT_RIGHT_KEY   = VK_RIGHT;    // right arrow
+const UCHAR GREEN_KNIGHT_UP_KEY      = VK_UP;       // up arrow
+const UCHAR GREEN_KNIGHT_DOWN_KEY    = VK_DOWN;     // down arrow
+
+const UCHAR RED_KNIGHT_LEFT_KEY = 0x41;
+const UCHAR RED_KNIGHT_RIGHT_KEY = 0x44;
+const UCHAR RED_KNIGHT_UP_KEY = 0x57;
+const UCHAR RED_KNIGHT_DOWN_KEY = 0x53;
+#endif
---
+++ Animation Example Class/SpaceshipControl/spacewar.cpp
@@ -0,0 +1,202 @@
+// Programming 2D Games
+// Copyright (c) 2011 by: 
+// Charles Kelly
+// Move spacejpo with arrow keys.
+// Chapter 5 spacewar.cpp v1.0
+// This class is the core of the game
+
+#include "spaceWar.h"
+
+//=============================================================================
+// Constructor
+//=============================================================================
+Spacewar::Spacewar()
+{}
+
+//=============================================================================
+// Destructor
+//=============================================================================
+Spacewar::~Spacewar()
+{
+	releaseAll();           // call onLostDevice() for every graphics item
+}
+
+//=============================================================================
+// Initializes the game
+// Throws GameError on error
+//=============================================================================
+void Spacewar::initialize(HWND hwnd)
+{
+	Game::initialize(hwnd); // throws GameError
+	if (!greenKnightTexture.initialize(graphics,"pictures\\greensoldiersheetupdate.png"))
+		throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing Player 1 texture"));
+
+	if (!greenKnight.initialize(graphics, 256/4, 384/6, GREEN_KNIGHT_COLS, &greenKnightTexture))
+		throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing Player 1"));
+	greenKnight.setX(GAME_WIDTH/4);                    // start above and left of planet
+	greenKnight.setY(GAME_HEIGHT/2);
+	greenKnight.setFrames(GREEN_KNIGHT_LOOKING_RIGHT_START, GREEN_KNIGHT_LOOKING_RIGHT_END);   // animation frames
+	greenKnight.setCurrentFrame(GREEN_KNIGHT_LOOKING_RIGHT_START);     // starting frame
+	greenKnight.setFrameDelay(GREEN_KNIGHT_ANIMATION_DELAY);
+
+	if (!redKnightTexture.initialize(graphics,"pictures\\redsoldiersheetupdate.png"))
+		throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing Player 2 texture"));
+
+	if (!redKnight.initialize(graphics, 256/4, 384/6, RED_KNIGHT_COLS, &redKnightTexture))
+		throw(GameError(gameErrorNS::FATAL_ERROR, "Error initializing Player 2"));
+	redKnight.setX(GAME_WIDTH/2);                    // start above and left of planet
+	redKnight.setY(GAME_HEIGHT/2);
+	redKnight.setFrames(RED_KNIGHT_LOOKING_LEFT_START, RED_KNIGHT_LOOKING_LEFT_END);   // animation frames
+	redKnight.setCurrentFrame(RED_KNIGHT_LOOKING_LEFT_START);     // starting frame
+	redKnight.setFrameDelay(RED_KNIGHT_ANIMATION_DELAY);
+
+	redLastDirection = left;
+	greenLastDirection = right;
+	keyDownLastFrame = false;
+	keyDownThisFrame = false;
+	return;
+}
+
+//=============================================================================
+// Update all game items
+//=============================================================================
+void Spacewar::update()
+{
+	int greenDirectionx = 0;
+	int greenDirectiony = 0;
+	int redDirectionx = 0;
+	int redDirectiony = 0;
+	if(input->isKeyDown(VK_ESCAPE)){
+		exit(0);
+	}
+	if(input->isKeyDown(GREEN_KNIGHT_RIGHT_KEY))            // if move right
+	{				
+		greenDirectionx--;
+		greenKnight.setFrames(GREEN_KNIGHT_WALKING_RIGHT_START, GREEN_KNIGHT_WALKING_RIGHT_END);
+		if (greenKnight.getX() > GAME_WIDTH)               // if off screen right
+			greenKnight.setX((float)-greenKnight.getWidth());   // position off screen left
+		greenLastDirection = right;
+	}
+	if(input->isKeyDown(GREEN_KNIGHT_LEFT_KEY))             // if move left
+	{
+		greenDirectionx++;
+		greenKnight.setFrames(GREEN_KNIGHT_WALKING_LEFT_START, GREEN_KNIGHT_WALKING_LEFT_END);
+		if (greenKnight.getX() < -greenKnight.getWidth())         // if off screen left
+			greenKnight.setX((float)GAME_WIDTH);           // position off screen right
+		greenLastDirection = left;
+	}
+	if(input->isKeyDown(GREEN_KNIGHT_DOWN_KEY)){
+		greenDirectiony--;
+		greenKnight.setFrames(GREEN_KNIGHT_WALKING_DOWN_START, GREEN_KNIGHT_WALKING_DOWN_END);
+		if(greenKnight.getY() < -greenKnight.getHeight())
+			greenKnight.setY((float)GAME_HEIGHT);
+		greenLastDirection = down;
+	}
+	if(input->isKeyDown(GREEN_KNIGHT_UP_KEY)){
+		greenDirectiony++;
+		greenKnight.setFrames(GREEN_KNIGHT_WALKING_UP_START, GREEN_KNIGHT_WALKING_UP_END);
+		if(greenKnight.getY() < -greenKnight.getHeight())
+			greenKnight.setY((float)GAME_HEIGHT);
+		greenLastDirection = up;
+	}
+	if(greenDirectionx == 0 && greenDirectiony == 0){
+		if(greenLastDirection == left){
+			greenKnight.setFrames(GREEN_KNIGHT_LOOKING_LEFT_START, GREEN_KNIGHT_LOOKING_LEFT_END);
+		}
+		if(greenLastDirection == right){
+			greenKnight.setFrames(GREEN_KNIGHT_LOOKING_RIGHT_START, GREEN_KNIGHT_LOOKING_RIGHT_END);
+		}
+	}
+	//Player 2 controls
+	if(input->isKeyDown(RED_KNIGHT_RIGHT_KEY))            // if move right
+	{				
+		redDirectionx--;
+		redKnight.setFrames(RED_KNIGHT_WALKING_RIGHT_START, RED_KNIGHT_WALKING_RIGHT_END);
+		if (redKnight.getX() > GAME_WIDTH)               // if off screen right
+			redKnight.setX((float)-redKnight.getWidth());   // position off screen left
+		redLastDirection = right;
+	}
+	if(input->isKeyDown(RED_KNIGHT_LEFT_KEY))             // if move left
+	{
+		redDirectionx++;
+		redKnight.setFrames(RED_KNIGHT_WALKING_LEFT_START, RED_KNIGHT_WALKING_LEFT_END);
+		if (redKnight.getX() < -redKnight.getWidth())         // if off screen left
+			redKnight.setX((float)GAME_WIDTH);           // position off screen right
+		redLastDirection = left;
+	}
+	if(input->isKeyDown(RED_KNIGHT_DOWN_KEY)){
+		redDirectiony--;
+		redKnight.setFrames(RED_KNIGHT_WALKING_DOWN_START, RED_KNIGHT_WALKING_DOWN_END);
+		if(redKnight.getY() < -redKnight.getHeight())
+			redKnight.setY((float)GAME_HEIGHT);
+		redLastDirection = down;
+	}
+	if(input->isKeyDown(RED_KNIGHT_UP_KEY)){
+		redDirectiony++;
+		redKnight.setFrames(RED_KNIGHT_WALKING_UP_START, RED_KNIGHT_WALKING_UP_END);
+		if(redKnight.getY() < -redKnight.getHeight())
+			redKnight.setY((float)GAME_HEIGHT);
+		redLastDirection = up;
+	}
+	if(redDirectionx == 0 && redDirectiony == 0){
+		if(redLastDirection == left){
+			redKnight.setFrames(RED_KNIGHT_LOOKING_LEFT_START, RED_KNIGHT_LOOKING_LEFT_END);
+		}
+		if(redLastDirection == right){
+			redKnight.setFrames(RED_KNIGHT_LOOKING_RIGHT_START, RED_KNIGHT_LOOKING_RIGHT_END);
+		}
+	}
+	redKnight.setX(redKnight.getX() - frameTime * RED_KNIGHT_SPEED * redDirectionx);
+	greenKnight.setX(greenKnight.getX() - frameTime * GREEN_KNIGHT_SPEED * greenDirectionx);
+	redKnight.setY(redKnight.getY() - frameTime * RED_KNIGHT_SPEED * redDirectiony);
+	greenKnight.setY(greenKnight.getY() - frameTime * GREEN_KNIGHT_SPEED * greenDirectiony);
+	greenKnight.update(frameTime);
+	redKnight.update(frameTime);
+}
+
+//=============================================================================
+// Artificial Intelligence
+//=============================================================================
+void Spacewar::ai()
+{}
+
+//=============================================================================
+// Handle collisions
+//=============================================================================
+void Spacewar::collisions()
+{}
+
+//=============================================================================
+// Render game items
+//=============================================================================
+void Spacewar::render()
+{
+	graphics->spriteBegin();                // begin drawing sprites
+	greenKnight.draw();                            // add the spacejpo to the scene
+	redKnight.draw();
+	graphics->spriteEnd();                  // end drawing sprites
+}
+
+//=============================================================================
+// The graphics device was lost.
+// Release all reserved video memory so graphics device may be reset.
+//=============================================================================
+void Spacewar::releaseAll()
+{
+	Game::releaseAll();
+	greenKnightTexture.onLostDevice();
+	redKnightTexture.onLostDevice();
+	return;
+}
+
+//=============================================================================
+// The grahics device has been reset.
+// Recreate all surfaces.
+//=============================================================================
+void Spacewar::resetAll()
+{
+	greenKnightTexture.onResetDevice();
+	redKnightTexture.onResetDevice();
+	Game::resetAll();
+	return;
+}
